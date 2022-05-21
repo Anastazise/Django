89980448c90ac3c59a70703ae788318b7fb5c474
Anastazise <anastazidevilfor@yandex.ru>
2022-05-21 22:53:24 +0300

    css

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index) #переход на главную страницу
+    path('', views.index), #переход на главную страницу
+    path('blogs', views.blogs),
+    path('login', views.login),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
+
+def blogs(request):
+    return render(request, 'main/blogs.html')
+
+def login(request):
+    return render(request, 'main/login.html')

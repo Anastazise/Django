49d198dadb021375c34bd1513a81ef436b9c1273
Anastazise <anastazidevilfor@yandex.ru>
2022-05-24 17:05:32 +0300

    абортыш

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# class Post(models.Model):
+#     title = models.CharField("Заголовок", max_length=50)
+#     text = models.TextField("Текст статьи", max_length=1500)
+#     image = models.ImageField("Изображение", upload_to="templates/news/image", blank=True)
+#     create = models.DateTimeField("Создан", auto_now_add=True)
+
+#     def __str__(self):
+#         return self.title

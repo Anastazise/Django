c64f364a533e084535e8e5477f2de21875309add
Anastazise <anastazidevilfor@yandex.ru>
2022-05-24 12:53:37 +0300

    add posts

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,15 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField("Заголовок", max_length=50)
+    text = models.TextField("Текст статьи", max_length=1500)
+    image = models.ImageField("Изображение", upload_to="post/", blank=True)
+    create = models.DateTimeField("Создан", auto_now_add=True)
+    update = models.DateTimeField("Обновлено", auto_now=True)
+    moder = models.BooleanField("Модерация", default=False)
+
+def __str__(self):
+    return self.title
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')) #отслеживание перехода на главную страницу
+    path('', include('main.urls')), #отслеживание перехода на главную страницу
+    path('news/', include('news.urls'))
 ]

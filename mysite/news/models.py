7fcfec5c33ee615966568b0bc2ae1464b7eb841c
Anastazise <anastazidevilfor@yandex.ru>
2022-05-31 22:19:37 +0300

    add admin

---
+++ mysite/news/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField("Заголовок", max_length=30)
+    text = models.TextField("Текст статьи", max_length= 1500)
+    image = models.ImageField("Изображение", upload_to="post/", blank=True)
+    create = models.DateTimeField("Время создания", auto_now_add=True)
+    blog = models.SlugField("Блог", max_length= 15)
+
+    def __str__(self):
+        return  self.title

74c5d6a0aff7cbb570d7dd643ee1008526e146e6
Anastazise <anastazidevilfor@yandex.ru>
2022-05-31 21:38:18 +0300

    абортыш 2

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,10 +1,14 @@
+from distutils.command.upload import upload
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
 
-# class Post(models.Model):
-#     title = models.CharField("Заголовок", max_length=50)
-#     text = models.TextField("Текст статьи", max_length=1500)
-#     image = models.ImageField("Изображение", upload_to="templates/news/image", blank=True)
-#     create = models.DateTimeField("Создан", auto_now_add=True)
+class News(models.Model):
+    title = models.CharField("Заголовок", max_length=30)
+    text = models.TextField("Текст статьи", max_length= 1500)
+    image = models.ImageField("Изображение", upload_to="post/", blank=True)
+    create = models.DateTimeField("Время создания", auto_now_add=True)
+    blog = models.SlugField("Блог", max_length= 15)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return  self.title

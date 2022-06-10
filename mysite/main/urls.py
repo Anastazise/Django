be030eb80708daff7c38988ac659e399ee0808a2
Anastazise <anastazidevilfor@yandex.ru>
2022-06-10 16:23:09 +0300

    2 commit

--- mysite/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index), #переход на главную страницу
-    path('blogs', views.blogs),
-    path('login', views.login),
-]
